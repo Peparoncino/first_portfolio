33e4fffc43ea5b92fdcf3d9d6db984097e50e08d
oreore <yuya.t@orenoMacBook-Air.local>
2026-05-30 18:33:23 +0900

    react-iconsの追加 contactのバッジをアイコンに変更

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,13 +1,18 @@
+import { FaGithub } from "react-icons/fa";
+import { CiMail } from "react-icons/ci";
+
 function Contact(){
   return(
     <section className="contact">
       <h2>Contact</h2>
       <div className="contact-links">
         <a className="contact-link" href="#" target="_blank" rel="noreferrer">
-          GitHub
+          <FaGithub size={20} />
+          {/* GitHub */}
         </a>
         <a className="contact-link" href="#">
-          お問い合わせ
+          <CiMail size={20} />
+          {/* お問い合わせ */}
         </a>
       </div>
     </section>
